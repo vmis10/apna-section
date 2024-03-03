ec1029b9776682d0d57a50cd8816cce49f1e12d7
vivek mishra <mishra.vvk93@gmail.com>
2024-03-03 13:41:24 +0530

    build

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="my-4 text-center text-muted small">
-      All rights reserved © 2024 Vivek Mishra IRSSE-2019.
+      All rights reserved © 2024.
     </footer>
   );
 }
