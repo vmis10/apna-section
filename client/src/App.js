d0bff7fa24e0ee51d9984eb847deb151415cbebe
vivek mishra <mishra.vvk93@gmail.com>
2024-03-03 15:33:34 +0530

    ext links

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import MainLayout from './MainLayout';
 import MainNavbar from './components/MainNavBar';
+import ExternalLinks from './components/ExternalLinks';
 import Footer from './components/Footer';
 import Signup from './components/Signup';
 import MaintCircular from './components/MaintCircular';
@@ -20,6 +21,7 @@ function App() {
           </Route>
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/externallinks" element={<ExternalLinks />} />
           <Route path="*" element={<Login />} />
         </Routes>
       </BrowserRouter>
---
+++ client/src/components/ExternalLinks.js
@@ -0,0 +1,18 @@
+import React from 'react';
+export default function ExternalLinks () {
+	return (
+		<div className="mt-3 px-4">
+			<li><a href="https://indianrailways.gov.in/railwayboard/view_section.jsp?lang=0&id=0,1,304,366,538,2713,2718,2722,2731" target="_blank">
+			<span>Camtech Handbook Year wise</span></a></li>
+			<li>
+				<a href="https://indianrailways.gov.in/railwayboard/view_section.jsp?lang=0&id=0,1,304,366,538,2713,2718,2722,2732" target="_blank">
+				<span>Camtech Handbook Subject wise</span></a>
+			</li>
+			<li>
+				<a href="https://rdso.indianrailways.gov.in/view_section.jsp?lang=0&id=0,4" target="_blank">
+				<span>RDSO Specifications</span></a>
+			</li>
+
+		</div>
+	);
+}
